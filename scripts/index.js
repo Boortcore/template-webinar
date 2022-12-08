7d39057e8cc7e21185fa123854f4a3c2f93534a8
Your Name <you@example.com>
2022-12-08 20:17:48 +0300

    Добавили использование шаблонов HTML

--- scripts/index.js
+++ scripts/index.js
@@ -8,21 +8,17 @@ const todos = [
   'Решить задачу на Codewars'
 ];
 
-
 const todosContainer = document.querySelector('.todos__list');
 const addTodoForm = document.querySelector('.todo-form');
 const input = addTodoForm.querySelector('.todo-form__input');
+const template = document.querySelector('#todo-item-template');
+
 const createTodo = (taskName) => {
-  const string = `<li class="todo-item">
-    <span class="todo-item__text"></span>
-    <button class="todo-item__edit"></button>
-    <button class="todo-item__copy"></button>
-    <button class="todo-item__del"></button>
-  </li>`
-  const container = document.createElement('div');
-  container.innerHTML = string;
-  container.querySelector('.todo-item__text').textContent = taskName;
-  return container.firstElementChild;
+  const task = template
+    .content.querySelector('.todo-item')
+    .cloneNode(true)
+  task.querySelector('.todo-item__text').textContent = taskName;
+  return task;
 }
 
 const renderTodo = (taskName) => {
